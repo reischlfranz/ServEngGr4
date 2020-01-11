c48f8964dfcb36ad432c1197b5beda2f1421a272
Reischl Franz <k11771360@students.jku.at>
2020-01-11 19:01:38 +0100

    After troubleshooting found reason why API calls failed

--- api/index.php
+++ api/index.php
@@ -14,8 +14,10 @@ header("Accept:application/json");
 // Catch the pre-flight request from browser
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 //  header("HTTP/1.1 200 ");
-  http_response_code(200);
-  header("Allow: OPTIONS, GET, POST, PUT, DELETE");
+  http_response_code(204);
+  header("Allow:*");
+  header("Access-Control-Allow-Methods:*");
+  header("Access-Control-Allow-Headers:*");
   exit;}
 
 // Read the request body (for POST/PUT methods - ignore for others):
